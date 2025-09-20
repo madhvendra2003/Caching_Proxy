6e5d3e5d267662f74bc883bf719908d0f8a43e9a
Madhvendra <madhv@madhvendra.>
2025-09-20 04:38:59 +0000

    controller message done

---
+++ src/main/java/com/madhvendra/caching/proxy/controller/UrlController.java
@@ -0,0 +1,29 @@
+package com.madhvendra.caching.proxy.controller;
+
+import com.madhvendra.caching.proxy.services.ProxyServices;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UrlController {
+    // here the endpoints will be defined
+    ProxyServices proxyServices;
+    public UrlController(ProxyServices proxyServices) {
+        this.proxyServices = proxyServices;
+    }
+
+
+    @RequestMapping(path = "/*")
+    public ResponseEntity<?> url (HttpServletRequest request) {
+      String reqUrl  = request.getRequestURI();
+      String reqMethod = request.getMethod();
+
+        Optional<Object> data  = proxyServices.getResponse(reqUrl, reqMethod);
+        return data.<ResponseEntity<?>>map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
--- src/main/java/com/madhvendra/caching/proxy/services/ProxyServices.java
+++ src/main/java/com/madhvendra/caching/proxy/services/ProxyServices.java
@@ -1,6 +1,11 @@
 package com.madhvendra.caching.proxy.services;
 
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProxyServices {
 
     // here the services will be implemented and will be called in  controller
+
 }
